127065805637d7651e587df5e7f120e2a55e0eb6
xwzhangcs <xwzhangcs@gmail.com>
2019-04-29 14:58:46 -0400

    minor changes

--- RoofResearch/main.cpp
+++ RoofResearch/main.cpp
@@ -22,6 +22,7 @@ void translate_bld_mask(const char* mask_tiff, std::string output_img_file);
 std::vector<std::vector<int>> read_tiff_int(const char* mask_tiff);
 void crop_img_from_mask(const char* src_tiff, std::vector<std::vector<int>> & type_info, std::string output_img_file);
 void findSkeleton(std::string src_img_file, std::string output_img_file);
+bool isClockwise(const std::vector<cv::Point>& polygon);
 
 int main(int argc, char** argv)
 {
@@ -37,51 +38,61 @@ int main(int argc, char** argv)
 
 void findSkeleton(std::string src_img_file, std::string output_img_file){
 	cv::Mat src = cv::imread(src_img_file, CV_LOAD_IMAGE_UNCHANGED);
-	cv::RNG rng(12345);
 	std::vector<std::vector<cv::Point> > contours;
+	std::vector<std::vector<cv::Point> > contours_approx;
 	std::vector<cv::Vec4i> hierarchy;
 	cv::findContours(src, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0));
 
-	/*cv::Mat drawing = cv::Mat::zeros(src.size(), CV_8UC3);
-	std::cout << "contours size is " << contours.size() << std::endl;
+	cv::Mat drawing = cv::Mat::zeros(src.size(), CV_8UC3);
+	contours_approx.resize(contours.size());
+	double epsilon = 4.0;
 	for (size_t i = 0; i< contours.size(); i++)
 	{
-	cv::Scalar color = cv::Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
-	drawContours(drawing, contours, (int)i, color, 2, 8, hierarchy, 0, cv::Point());
+		cv::Scalar color = cv::Scalar(255, 255, 255);
+		drawContours(drawing, contours, (int)i, color, 1, 8, hierarchy, 0, cv::Point());
+		cv::approxPolyDP(contours[i], contours_approx[i], epsilon, true);
 	}
-	cv::namedWindow("Contours", cv::WINDOW_AUTOSIZE);
+
+	for (size_t i = 0; i< contours_approx.size(); i++)
+	{
+		cv::Scalar color = cv::Scalar(255, 0, 0);
+		drawContours(drawing, contours_approx, (int)i, color, 1, 8, hierarchy, 0, cv::Point());
+	}
+	/*cv::namedWindow("Contours", cv::WINDOW_AUTOSIZE);
 	imshow("Contours", drawing);
 	cv::waitKey(0);*/
-	{
-		std::vector<std::vector<cv::Point> > contours;
-		std::vector<cv::Point> contour;
-		contour.push_back(cv::Point(50, 50));
-		contour.push_back(cv::Point(150, 50));
-		contour.push_back(cv::Point(150, 150));
-		contour.push_back(cv::Point(100, 150));
-		contour.push_back(cv::Point(100, 100));
-		contour.push_back(cv::Point(50, 100));
-		contours.push_back(contour);
-		cv::Mat drawing = cv::Mat::zeros(cv::Size(200, 200), CV_8UC3);
-		for (size_t i = 0; i< contours.size(); i++)
-		{
-			cv::Scalar color = cv::Scalar(255, 0, 0);
-			drawContours(drawing, contours, (int)i, color, 1, 8, hierarchy, 0, cv::Point());
-		}
-		Polygon_2 poly;
-		for (int i = 0; i < contours[0].size(); i++)
-			poly.push_back(Point(contours[0][i].x, contours[0][i].y));
-
-		// You can pass the polygon via an iterator pair
-		SsPtr iss =  CGAL::create_interior_straight_skeleton_2(poly.vertices_begin(), poly.vertices_end());
-		std::vector<std::vector<double>> edges = edges_straight_skeleton(*iss);
-		for (int i = 0; i< edges.size(); i++)
-		{
-			cv::Scalar color = cv::Scalar(0, 0, 255);
-			cv::line(drawing, cv::Point(edges[i][0], edges[i][1]), cv::Point(edges[i][2], edges[i][3]), color, 1, 8, 0);
-		}
-		cv::imwrite(output_img_file, drawing);
+	if (isClockwise(contours_approx[0])){
+		std::cout << "clockwise" << std::endl;
+		std::reverse(contours_approx[0].begin(), contours_approx[0].end());
 	}
+	else{
+		std::cout << "counterClockwise" << std::endl;
+	}
+	Polygon_2 poly;
+	std::cout << "There are " << contours_approx[0].size() << " points." << std::endl;
+	for (int i = 0; i < contours_approx[0].size(); i++)
+		poly.push_back(Point(contours_approx[0][i].x, contours_approx[0][i].y));
+
+	// You can pass the polygon via an iterator pair
+	SsPtr iss = CGAL::create_interior_straight_skeleton_2(poly.vertices_begin(), poly.vertices_end());
+	std::vector<std::vector<double>> edges = edges_straight_skeleton(*iss);
+	for (int i = 0; i< edges.size(); i++)
+	{
+		cv::Scalar color = cv::Scalar(0, 0, 255);
+		cv::line(drawing, cv::Point(edges[i][0], edges[i][1]), cv::Point(edges[i][2], edges[i][3]), color, 1, 8, 0);
+	}
+	cv::imwrite(output_img_file, drawing);
+
+	
+}
+
+bool isClockwise(const std::vector<cv::Point>& polygon) {
+	float S = 0;
+	for (int i = 0; i < polygon.size(); i++) {
+		int next = (i + 1) % polygon.size();
+		S += (polygon[next].x - polygon[i].x) * (polygon[next].y + polygon[i].y);
+	}
+	return S > 0;
 }
 
 std::vector<std::vector<float>> read_tiff_float(const char* type_tiff){
