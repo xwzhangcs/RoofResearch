85d270ff623ce7c4ab7a0eb98817739d13f0e49a
Zhang <zhan2597@purdue.lcl>
2019-09-02 09:34:10 -0400

    minor changes

--- RoofResearch/main.cpp
+++ RoofResearch/main.cpp
@@ -11,99 +11,12 @@
 #include "RoofUtils.h"
 
 std::vector<int> adjust_chip(cv::Mat chip);
+void generate_test_img(std::string path, int res_src, int res_dst, int padding, bool bDownSampling);
 
 int main(int argc, char** argv)
 {
 	std::string path = "../data/ex7";
-	cv::Mat src = cv::imread(path + "/src.png", CV_LOAD_IMAGE_UNCHANGED);
-	bool bChanged = false;
-	if (src.channels() == 4){
-		cv::cvtColor(src, src, CV_RGBA2GRAY);
-		bChanged = true;
-	}
-	if (src.channels() == 3){
-		cv::cvtColor(src, src, CV_RGB2GRAY);
-		bChanged = true;
-	}
-	if (bChanged){
-		// correct the color
-		for (int i = 0; i < src.size().height; i++) {
-			for (int j = 0; j < src.size().width; j++) {
-				//noise
-				if ((int)src.at<uchar>(i, j) < 128) {
-					src.at<uchar>(i, j) = (uchar)0;
-				}
-				else
-					src.at<uchar>(i, j) = (uchar)255;
-			}
-		}
-		cv::imwrite(path + "/correct.png", src);
-	}
-	/*cv::Mat dst;
-	cv::Point2f pt(src.cols / 2., src.rows / 2.);
-	cv::Mat r = cv::getRotationMatrix2D(pt, 4, 1.0);
-	warpAffine(src, dst, r, cv::Size(src.cols, src.rows));
-	cv::imwrite(path + "/rotate.png", dst);
-	return 0;*/
-	std::vector<int> boundaries = adjust_chip(src);
-	cv::Mat adjust_img = src(cv::Rect(boundaries[2], boundaries[0], boundaries[3] - boundaries[2] + 1, boundaries[1] - boundaries[0] + 1));
-	cv::imwrite(path + "/asjust.png", adjust_img);
-	// resize to 10 by 10
-	int target_small_width = 8;
-	int target_small_height = 8;
-	int target_big_width = 200;
-	int target_big_height = 200;
-	int step = 25;
-	int padding_size = 12;
-	//{
-	//	cv::Mat big_img;
-	//	cv::resize(adjust_img, big_img, cv::Size(target_big_width, target_big_height));
-	//	// correct the color
-	//	for (int i = 0; i < big_img.size().height; i++) {
-	//		for (int j = 0; j < big_img.size().width; j++) {
-	//			//noise
-	//			if ((int)big_img.at<uchar>(i, j) < 128) {
-	//				big_img.at<uchar>(i, j) = (uchar)0;
-	//			}
-	//			else
-	//				big_img.at<uchar>(i, j) = (uchar)255;
-	//		}
-	//	}
-	//	cv::imwrite(path + "/big.png", big_img);
-	//	// add padding
-	//	int borderType = cv::BORDER_CONSTANT;
-	//	cv::Mat img_padding;
-	//	cv::copyMakeBorder(big_img, img_padding, padding_size, padding_size, padding_size, padding_size, borderType, cv::Scalar(0, 0, 0));
-	//	cv::imwrite(path + "/final.png", img_padding);
-	//	system("pause");
-	//}
-	//return 0;
-	cv::Mat small_img;
-	cv::resize(adjust_img, small_img, cv::Size(target_small_width, target_small_height));
-	// correct color
-	for (int i = 0; i < small_img.size().height; i++) {
-		for (int j = 0; j < small_img.size().width; j++) {
-			//noise
-			if ((int)small_img.at<uchar>(i, j) < 200) {
-				small_img.at<uchar>(i, j) = (uchar)0;
-			}
-			else
-				small_img.at<uchar>(i, j) = (uchar)255;
-		}
-	}
-	cv::imwrite(path + "/small.png", small_img);
-	cv::Mat big_img(target_big_width, target_big_height, CV_8UC1);;
-	for (int i = 0; i < big_img.size().height; i++) {
-		for (int j = 0; j < big_img.size().width; j++) {
-			big_img.at<uchar>(i, j) = small_img.at<uchar>(i / step, j / step);
-		}
-	}
-	cv::imwrite(path + "/big.png", big_img);
-	// add padding
-	int borderType = cv::BORDER_CONSTANT;
-	cv::Mat img_padding;
-	cv::copyMakeBorder(big_img, img_padding, padding_size, padding_size, padding_size, padding_size, borderType, cv::Scalar(0, 0, 0));
-	cv::imwrite(path + "/final.png", img_padding);
+	generate_test_img(std::string path, int res_src, int res_dst, int padding, false);
 	system("pause");
 	return 0;
 }
@@ -192,4 +105,91 @@ std::vector<int> adjust_chip(cv::Mat chip) {
 	boundaries[2] = pos_left;
 	boundaries[3] = pos_right;
 	return boundaries;
+}
+
+void generate_test_img(std::string path, int res_src, int res_dst, int padding, bool bDownSampling){
+	cv::Mat src = cv::imread(path + "/src.png", CV_LOAD_IMAGE_UNCHANGED);
+	bool bChanged = false;
+	if (src.channels() == 4){
+		cv::cvtColor(src, src, CV_RGBA2GRAY);
+		bChanged = true;
+	}
+	if (src.channels() == 3){
+		cv::cvtColor(src, src, CV_RGB2GRAY);
+		bChanged = true;
+	}
+	if (bChanged){
+		// correct the color
+		for (int i = 0; i < src.size().height; i++) {
+			for (int j = 0; j < src.size().width; j++) {
+				//noise
+				if ((int)src.at<uchar>(i, j) < 128) {
+					src.at<uchar>(i, j) = (uchar)0;
+				}
+				else
+					src.at<uchar>(i, j) = (uchar)255;
+			}
+		}
+		cv::imwrite(path + "/correct.png", src);
+	}
+	std::vector<int> boundaries = adjust_chip(src);
+	cv::Mat adjust_img = src(cv::Rect(boundaries[2], boundaries[0], boundaries[3] - boundaries[2] + 1, boundaries[1] - boundaries[0] + 1));
+	cv::imwrite(path + "/asjust.png", adjust_img);
+
+	int target_small_width = res_src;
+	int target_small_height = res_src;
+	int target_big_width = res_dst;
+	int target_big_height = res_dst;
+	int step = res_dst / res_src;
+	int padding_size = padding;
+
+	if (!bDownSampling){
+		cv::Mat big_img;
+		cv::resize(adjust_img, big_img, cv::Size(target_big_width, target_big_height));
+		// correct the color
+		for (int i = 0; i < big_img.size().height; i++) {
+			for (int j = 0; j < big_img.size().width; j++) {
+				//noise
+				if ((int)big_img.at<uchar>(i, j) < 200) {
+					big_img.at<uchar>(i, j) = (uchar)0;
+				}
+				else
+					big_img.at<uchar>(i, j) = (uchar)255;
+			}
+		}
+		cv::imwrite(path + "/big.png", big_img);
+		// add padding
+		int borderType = cv::BORDER_CONSTANT;
+		cv::Mat img_padding;
+		cv::copyMakeBorder(big_img, img_padding, padding_size, padding_size, padding_size, padding_size, borderType, cv::Scalar(0, 0, 0));
+		cv::imwrite(path + "/final.png", img_padding);
+	}
+	else{
+		cv::Mat small_img;
+		cv::resize(adjust_img, small_img, cv::Size(target_small_width, target_small_height));
+		// correct color
+		for (int i = 0; i < small_img.size().height; i++) {
+			for (int j = 0; j < small_img.size().width; j++) {
+				//noise
+				if ((int)small_img.at<uchar>(i, j) < 200) {
+					small_img.at<uchar>(i, j) = (uchar)0;
+				}
+				else
+					small_img.at<uchar>(i, j) = (uchar)255;
+			}
+		}
+		cv::imwrite(path + "/small.png", small_img);
+		cv::Mat big_img(target_big_width, target_big_height, CV_8UC1);;
+		for (int i = 0; i < big_img.size().height; i++) {
+			for (int j = 0; j < big_img.size().width; j++) {
+				big_img.at<uchar>(i, j) = small_img.at<uchar>(i / step, j / step);
+			}
+		}
+		cv::imwrite(path + "/big.png", big_img);
+		// add padding
+		int borderType = cv::BORDER_CONSTANT;
+		cv::Mat img_padding;
+		cv::copyMakeBorder(big_img, img_padding, padding_size, padding_size, padding_size, padding_size, borderType, cv::Scalar(0, 0, 0));
+		cv::imwrite(path + "/final.png", img_padding);
+	}
 }
